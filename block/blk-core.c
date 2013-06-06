65b84d72bae121582c2a422f307f5789596785bc
Paul Reioux <reioux@gmail.com>
2013-06-06 01:24:16 -0500

    block/blk-core: add support for Linux 3.0.x for urgent request handling
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- block/blk-core.c
+++ block/blk-core.c
@@ -309,9 +309,9 @@ void __blk_run_queue(struct request_queue *q)
 		return;
 
 	if (!q->notified_urgent &&
-		q->elevator->type->ops.elevator_is_urgent_fn &&
+		q->elevator->elevator_type->ops.elevator_is_urgent_fn &&
 		q->urgent_request_fn &&
-		q->elevator->type->ops.elevator_is_urgent_fn(q)) {
+		q->elevator->elevator_type->ops.elevator_is_urgent_fn(q)) {
 		q->notified_urgent = true;
 		q->urgent_request_fn(q);
 	} else
